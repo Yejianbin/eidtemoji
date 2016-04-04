d7c2338e5acb9e88121be7c1be8406f09f1946ee
YeXiaoBin <695831115@qq.com>
2016-04-04 14:12:49 +0800

    First commit

---
+++ app/src/main/java/com/editemoji/app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.editemoji.app;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.select_image);
+    }
+}
